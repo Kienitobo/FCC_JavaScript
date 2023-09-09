33bca9920a69c7a005c2b6fee0cc50421436ca6c
Kieni Edojah <105279859+Kienitobo@users.noreply.github.com>
2023-09-09 12:31:07 -0400

    More ES6 practice

--- ES6_var_let_scope.js
+++ ES6_var_let_scope.js
@@ -303,3 +303,39 @@ class Vegetable {
 
 const carrot = new Vegetable("carrot");
 console.log(carrot.name); // Should display 'carrot'
+
+//Use getters and setters to Control Access to an Object
+/*Use the class keyword to create a Thermostat class. 
+The constructor accepts a Fahrenheit temperature.
+
+In the class, create a getter to obtain the temperature in 
+Celsius and a setter to set the temperature in Celsius.
+
+Remember that C = 5/9 * (F - 32) and F = C * 9.0 / 5 + 32, 
+where F is the value of temperature in Fahrenheit, and C is the 
+value of the same temperature in Celsius.
+
+Note: When you implement this, you will track the temperature 
+inside the class in one scale, either Fahrenheit or Celsius.
+*/
+// Only change code below this line
+class Thermostat{
+  constructor(fahrenheit){
+    this._temperature = 5/9*(fahrenheit -32);
+  }
+  get temperature(){
+    return this._temperature;
+    
+  }
+  set temperature(CelsiusTemp){
+    this._temperature = CelsiusTemp;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+console.log(temp);
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+console.log(temp);
